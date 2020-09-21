c6eca0efaf55d2b92c6aca54a3be0bc46adbe71d
1stmuse <olysegs@gmail.com>
2020-09-20 21:04:26 -0400

    stated building the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,91 +1,35 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom'
 
 const Home = ({history}) => {
-    const [name1, setName1] = useState('')
-    const [name2, setName2] = useState('')
-    const [doc1, setDoc1] = useState('')
-    const [doc2, setDoc2] = useState('')
-    const [error, setError] = useState(false)
-    const [result, setResult] = useState(null)
-    const [similarWord,setWord] = useState([])
+    const [room, setRoom] = useState('')
+    const [rooms, setRooms] = useState([{id:3,name:'james'}, {id:2,name:'james'}, {id:1,name:'james'}])
 
-    const handleUpload =(e, doc)=>{
-        const file = e.target.files[0]
-        const fileReader = new FileReader(file)
-        fileReader.readAsDataURL(file);
-        fileReader.onload= e=>{
-            if(doc ==='doc1'){
-                setDoc1(fileReader.result)
-            }else if(doc ==='doc2'){
-                setDoc2(fileReader.result)
-            }else{
-                return false
-            }
-        }
-    }
-    const handleCompare =()=>{
-        if(!doc1.length && !doc2.length){
-            setError(true)
-            return false
-        }else{
-            fetch('https://0693ee4b-098e-4c78-8010-66e96299af11.mock.pstmn.io/compare',{
-                method:'POST',
-                headers:{'Content-Type':'application/json'},
-                body:JSON.stringify({
-                    doc1,
-                    doc2
-                })
-            })
-            .then(res=>res.json())
-            .then(result=>{
-              setResult(result.similarity)
-              setWord(result.similarChars)
-            }).catch(err=>{
-                alert(err)
-            })
-            setError(false)
-            console.log(doc1 , doc2)
-        }
-    }
     return (
         <div className='home'>
-            <div className='home-main'>
-                <div className='get-started' style={{textAlign:'center'}} >
-                    <h2>Get started</h2>
+            <div className='welcome'><h2>Welcome Akinnagbe!! join a room or create one to start chatting</h2></div>
+            <div className='roomsDiv'>
+                <div className='rooms'>
+                    {
+                        rooms.length === 0 ?
+                             <div>no rooms create, create one</div> :
+                             rooms.map(room=>(
+                                 <div key={room.id} className='room'>
+                                    <Link className='link'>{room.name} </Link>
+                                </div>
+                            ))
+                    }
                 </div>
-                <div className='compare-div' >
-                    <div>
-                        <h3>First Person</h3>
-                        <input type='text' 
-                            value={name1} 
-                            onChange={(e)=>setName1(e.target.value)}
-                        />
-                        <label htmlFor='file' className='label'>Upload document</label>
-                        <input type='file' id='file' onChange={(e)=>handleUpload(e, 'doc1')} />
+                <div className='roomForm'>
+                    <div className='add'>
+                        <h3>Create chatroom</h3>
                     </div>
-                    <div>
-                        <h3>Second Person</h3>
-                        <input type='text' 
-                            value={name2} 
-                            onChange={(e)=>setName2(e.target.value)}
-                        />
-                        <label htmlFor='file2' className='label' >Upload document</label>
-                        <input type='file' id='file2' onChange={(e)=>handleUpload(e,'doc2')}/>
+                    <div classname='form-div'>
+                        <div className='may'>
+                            <input type='text' className='roomName'/>
+                        </div>
+                        <div className='addBtn'>Create</div>
                     </div>
-                </div>
-                <div className='result-div' style={{display: result===null? 'none' : 'block'}}>
-                    <p>Percentage of Comparison</p>
-                    <div className='result' style={{margin:'0 auto', border: result >=50 ?'2px solid green' : '2px solid yellow'}} >
-                        <p>{result}% </p>
-                    </div>
-                    {similarWord.length ? <p>Similar words are {similarWord.toString()} </p>: null}
-                </div>
-                <div className='compare-btn'>
-                    <div onClick={handleCompare} >Compare</div>
-                    <div onClick={()=>history.push('/')} >Logout</div>
-                </div>
-                <div className='error' style={{opacity:error ? 1 : 0}} >
-                    Sorry you cannot compare empty documents
                 </div>
             </div>
         </div>
