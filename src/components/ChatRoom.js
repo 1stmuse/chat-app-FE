dd8a9691a8dca0729c74f4e8f510321465bed020
1stmuse <olysegs@gmail.com>
2020-12-31 01:20:57 +0100

    added show available members functionality and some styles

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client'
-import Alert from './Alert'
-import {useHistory, useRouteMatch} from 'react-router-dom'
+import { useRouteMatch} from 'react-router-dom'
 
 const ChatRoom = ({socket}) => {
     const history = useRouteMatch()
@@ -9,32 +7,14 @@ const ChatRoom = ({socket}) => {
     const userName = localStorage.getItem('user')
     const [message, setMessage] =useState('')
     const [messages,setMessages]= useState([])
+    const [members, setMembers] = useState([])
+    const token = localStorage.getItem('token')
     
-    // const [socket, setSocket] =useState(null)
-
-    // const socketInit = ()=>{
-    //     const token = localStorage.getItem('token')
-    //     const newSock = io('http://localhost:8000', {
-    //         query:{
-    //             id:token
-    //         }
-    //     })
-  
-    //     newSock.on('connect', (message)=>{
-    //         Alert('success', 'connected')
-    //     })
-    //     newSock.on('mess',(mess)=>console.log(mess))
-    //     newSock.on('disconnection',()=>{
-    //         setSocket(null)
-    //         Alert('error', 'socket disconnected')
-    //     })
-        
-    //     setSocket(newSock)
-    // }
-
-    // useEffect(()=>{
-    //     socketInit()
-    // },[])
+    useEffect(()=>{
+        socket.on('members', (members)=>{
+            setMembers((prev)=> [...members, ...prev ])
+        })
+    },[])
 
     const addMessage=(e)=>{
         e.preventDefault()
@@ -49,7 +29,7 @@ const ChatRoom = ({socket}) => {
         socket.on('new-message', (message)=>{
             setMessages(prev=> [...prev, message])
         })
-    }, [])
+    }, [0])
 
     useEffect(()=>{
        if(socket){
@@ -59,40 +39,53 @@ const ChatRoom = ({socket}) => {
         return ()=>{
             socket.emit('leaveRoom', roomId)
         }
-    },[])
+    },[0])
 
-    // console.log(history.params.id)
     return (
         <div>
             <header className='header'>
                 <h1 style={{marginRight:'15px'}} >Let's Talk</h1>
                 <p className="fa fa-comments fa-2x" aria-hidden="true"></p>
             </header>
-            <div className='chat-main'>
-                <div className='chat-header'>
-                    <div className='chat-logo'>
-                        <p className='ball' ></p>
-                        <h3>Let's Talk</h3>
+            <section className='chat-section'>
+                <div className='members'>
+                    <div className='members-label'>
+                        Available members
+                    </div>
+                    <div className='avail-members'>
+                        {
+                            members.length > 0 ?
+                            members.map((member, i)=><div key={i} className="memb-names"> <p className='online'></p> <p >{member} </p></div>) :
+                            <p>Ooops group is empty </p>
+                        }
                     </div>
                 </div>
-                <div className='chat-body'>
-                    {messages && messages.map(message=>(
-                        <div className={message.name.trim().toLowerCase() === userName.toLowerCase() ? 'mychat' : 'others'}>
-                           <div className={message.name.trim().toLowerCase() === userName.toLowerCase() ? 'alias' : 'o-alias'}>
-                               {message.name[0].toUpperCase()}
-                           </div>
-                           <div className='a-msg'>{message.message} </div>
+                <div className='chat-main'>
+                    <div className='chat-header'>
+                        <div className='chat-logo'>
+                            <p className='ball' ></p>
+                            <h3>Let's Talk</h3>
                         </div>
-                    ))}
+                    </div>
+                    <div className='chat-body'>
+                        {messages && messages.map(message=>(
+                            <div key={message.message.id} className={message.name.trim().toLowerCase() === userName.toLowerCase() ? 'mychat' : 'others'}>
+                                <div className={message.name.trim().toLowerCase() === userName.toLowerCase() ? 'alias' : 'o-alias'}>
+                                    {message.name[0].toUpperCase()}
+                                </div>
+                                <div className='a-msg'>{message.message.message} </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='chat-input-div'>
+                        <input type='text' className='chat-input' 
+                            value={message} 
+                            onChange={(e)=> addMessage(e)} 
+                        />
+                        <div className='chat-btn'  onClick={sendMessage}>Send</div>
+                    </div>
                 </div>
-                <div className='chat-input-div'>
-                    <input type='text' className='chat-input' 
-                        value={message} 
-                        onChange={(e)=> addMessage(e)} 
-                    />
-                    <div className='chat-btn'  onClick={sendMessage}>Send</div>
-                </div>
-            </div>
+            </section>
         </div>
     );
 };
